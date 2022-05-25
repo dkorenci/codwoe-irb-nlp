c90b386393b0fcf612dc31895ed11a7f36b6b42a
damirko <damir.korencic@gmail.com>
2022-05-25 18:10:24 +0200

    settings-template.py - added description of the resource folders

--- settings-template.py
+++ settings-template.py
@@ -1,3 +1,9 @@
+# location of the training/dev/test dataset, in the organizers' json format
 dataset_folder = ''
+
+# cache for the constructed sentencepiece tokenizers
 lang_resources_folder = ''
+
+# cache for the constructed glove vectors, and place
+#  where the compiled glove command line is located
 glove_folder = ''
